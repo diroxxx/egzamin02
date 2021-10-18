d9d6b50e8dec888d01bd8c667e0f6db0ed0b2ed6
diroxxx <che.dom29@gmail.com>
2021-10-18 14:47:22 +0200

    php

---
+++ liga.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>Piłka Nożna</title>
+</head>
+<body>
+    <header class="baner" >
+        <h3>
+            Reprezentacja polski w piłce nożnej
+        </h3>
+        <img src="obraz1.png" alt="reprezentacja">
+    </header>
+    <section class="lewa" ></section>
+    <section class="prawy"> </section>
+    <main class="glowny"></main>
+    <section class="liga"></section>
+</body>
+</html>
